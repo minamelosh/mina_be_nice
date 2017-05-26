e4e8522dbf006aab3990a69d00630433899f4414
Mina Melosh <email": null>
2017-05-26 14:20:37 -0700

    create delete buttons for images on a users homepage.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,5 +1,14 @@
 get '/images/top_five' do
   @top_five = Giphy.trending(limit: 5)
-
   erb :'images/top_five'
+end
+
+#route to delete an image of user page
+get '/users/:user_id/image/:id/delete' do
+  @image = Image.find(params[:id])
+  @image.destroy
+  @user = User.find(params[:user_id])
+  # @messages = ["Your Image was deleted!"]
+  # redirect "/users/#{current_user.id}"
+  erb :'users/show'
 end
